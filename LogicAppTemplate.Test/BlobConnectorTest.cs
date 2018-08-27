9180002d062aebc39dee11605b3e4e2b91fd635a
Mattias Lögdberg <m.logdberg@gmail.com>
2018-08-27 09:48:22 +0200

    added blobtest

---
+++ LogicAppTemplate.Test/BlobConnectorTest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json.Linq;
+using System.Linq;
+
+namespace LogicAppTemplate.Test
+{
+    [TestClass]
+    public class BlobConnectorTest
+    {
+
+        private JObject GetTemplate()
+        {
+            var generator = new TemplateGenerator("INT0012C.Workday.Rehire.Leavers", "c107df29-a4af-4bc9-a733-f88f0eaa4296", "blobtest", new MockResourceCollector("BlobConnector"));
+
+            return generator.GenerateTemplate().GetAwaiter().GetResult();
+        }
+
+        [TestMethod]
+        public void GenreateTemplateTest()
+        {
+            var defintion = GetTemplate();
+            Assert.IsNotNull(defintion);
+        }
+        [TestMethod]
+        public void BlobConnectionTest()
+        {
+            var defintion = GetTemplate();
+
+            var connection = defintion.Value<JArray>("resources").Where(jj => jj.Value<string>("type") == "Microsoft.Web/connections" && jj.Value<string>("name") == "[parameters('azureblob_name')]").First();
+            Assert.AreEqual("[parameters('logicAppLocation')]", connection.Value<string>("location"));
+            Assert.AreEqual("[concat('/subscriptions/',subscription().subscriptionId,'/providers/Microsoft.Web/locations/',parameters('logicAppLocation'),'/managedApis/azureblob')]", connection["properties"]["api"].Value<string>("id"));
+
+            Assert.AreEqual("[parameters('azureblob_displayName')]", connection["properties"].Value<string>("displayName"));
+            Assert.AreEqual("[parameters('azureblob_accountName')]", connection["properties"]["parameterValues"].Value<string>("accountName"));
+            Assert.AreEqual("[listKeys(resourceId('Microsoft.Storage/storageAccounts', parameters('azureblob_accountName')), providers('Microsoft.Storage', 'storageAccounts').apiVersions[0]).keys[0].value]", connection["properties"]["parameterValues"].Value<string>("accessKey"));
+
+        }
+    }
+}
---
+++ LogicAppTemplate.Test/TableConnectorTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LogicAppTemplate.Test
+{
+    [TestClass]
+    public class TableConnectorTest
+    {
+        [TestMethod]
+        public void TestTableConnector()
+        {
+            var generator = new TemplateGenerator("addDataToTable", "c107df29-a4af-4bc9-a733-f88f0eaa4296", "blobtest", new MockResourceCollector("TableConnector"));
+
+            var defintion = generator.GenerateTemplate().GetAwaiter().GetResult();
+
+            Assert.AreEqual("[concat('/subscriptions/',subscription().subscriptionId,'/resourceGroups/',parameters('Billogram-ResourceGroup'),'/providers/Microsoft.Web/customApis/Billogram')]", defintion["resources"][0]["properties"]["parameters"]["$connections"]["value"]["Billogram"]["id"]);
+
+            Assert.AreEqual("Microsoft.Web/connections", defintion["resources"][1]["type"]);
+            Assert.AreEqual("[parameters('logicAppLocation')]", defintion["resources"][1]["location"]);
+            Assert.AreEqual("[parameters('Billogram_name')]", defintion["resources"][1]["name"]);
+            //subscriptions/89d02439-770d-43f3-9e4a-8b910457a10c/resourceGroups/Messaging/providers/Microsoft.Web/customApis/Billogram
+            //subscriptions/fakeecb73-d0ff-455d-a2bf-eae0b300696d/providers/Microsoft.Web/locations/westeurope/managedApis/filesystem
+            Assert.AreEqual("[concat('/subscriptions/',subscription().subscriptionId,'/resourceGroups/',parameters('Billogram-ResourceGroup'),'/providers/Microsoft.Web/customApis/Billogram')]", defintion["resources"][1]["properties"]["api"]["id"]);
+        }
+    }
+}
