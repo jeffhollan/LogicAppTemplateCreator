f32df70d6b16d0201ab7100e0d22a20d9c038ace
Mötz Jensen <motz@e-s.dk>
2019-07-07 20:44:09 +0200

    Import working version

---
+++ LogicAppTemplate.Test/VariableUpdaterTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+using Newtonsoft.Json.Linq;
+
+namespace LogicAppTemplate.Test
+{
+    [TestClass]
+    public class UpdateTemplateVariableTests
+    {
+        [TestMethod]
+        public void CreateUpdateTemplateVariableToValueClass()
+        {
+            new UpdateTemplateVariableReferenceToValue("","","");
+        }
+
+        [TestMethod]
+        public void GenerateParameterFileFromTemplate()
+        {
+            var content = GetEmbededFileContent("LogicAppTemplate.Test.TestFiles.VariableReference.json");
+            var generator = new UpdateTemplateVariableReferenceToValue("", "URL", "https://www.nationalbanken.dk/");
+
+            var definition = generator.UpdateTemplateVariable(content);
+            Assert.AreEqual(definition["parameters"]["HTTP-URI"]["defaultValue"], (JValue)"https://www.nationalbanken.dk/_vti_bin/DN/DataService.svc/CurrencyRatesXML?lang=da");
+        }
+
+        private static string GetEmbededFileContent(string resourceName)
+        {
+            var assembly = System.Reflection.Assembly.GetExecutingAssembly();
+
+
+            using (Stream stream = assembly.GetManifestResourceStream(resourceName))
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                return reader.ReadToEnd();
+            }
+
+        }
+
+    }
+}
---
+++ LogicAppTemplate/UpdateTemplateVariableToValueCmdlet.cs
@@ -0,0 +1,64 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management.Automation;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using LogicAppTemplate.Models;
+using Newtonsoft.Json;
+using System.Dynamic;
+using System.IO;
+
+namespace LogicAppTemplate
+{
+    [Cmdlet(VerbsData.Update, "TemplateVariableReferenceToValue", ConfirmImpact = ConfirmImpact.None)]
+    public class UpdateTemplateVariableReferenceToValue : PSCmdlet
+    {
+        [Parameter(
+            Mandatory = true,
+            HelpMessage = "The path to the template file"
+            )]
+        [Alias("Path")]
+        public string TemplateFile;
+
+        [Parameter(
+            Mandatory = true,
+            HelpMessage = "Name of the variable reference to switch out"
+            )]
+        [Alias("Name")]
+        public string Variable;
+        
+        [Parameter(
+            Mandatory = true,
+            HelpMessage = "Value that should be inserted instead of the current variable reference"
+            )]
+        public string Value;
+
+        public UpdateTemplateVariableReferenceToValue()
+        {
+
+        }
+
+        public UpdateTemplateVariableReferenceToValue(string TemplateFile, string Variable, string Value)
+        {
+            this.TemplateFile = TemplateFile;
+            this.Variable = Variable;
+            this.Value = Value;
+        }
+
+        public JObject UpdateTemplateVariable(string logicAppTemplateJson)
+        {
+            string pattern = string.Format("@{{variables('{0}')}}", Variable);
+            return JObject.Parse(logicAppTemplateJson.Replace(pattern, Value));
+        }
+
+        protected override void ProcessRecord()
+        {
+            string logicappTemplate = File.ReadAllText(TemplateFile);
+            JObject result = UpdateTemplateVariable(logicappTemplate);
+            WriteObject(result.ToString());
+        }
+    }
+}
