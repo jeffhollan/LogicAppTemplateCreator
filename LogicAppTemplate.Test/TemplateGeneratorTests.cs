6be8f90d7ebcdcb67a66daa166c0768ea6c3c9d1
Mattias <m.logdberg@gmail.com>
2020-06-22 13:35:16 +0200

    fixed test for custom connector

--- LogicAppTemplate.Test/TemplateGeneratorTests.cs
+++ LogicAppTemplate.Test/TemplateGeneratorTests.cs
@@ -646,14 +646,14 @@ namespace LogicAppTemplate.Tests
 
             var defintion = generator.GenerateTemplate().GetAwaiter().GetResult();
 
-            Assert.AreEqual("[concat('/subscriptions/',subscription().subscriptionId,'/resourceGroups/',parameters('Billogram-ResourceGroup'),'/providers/Microsoft.Web/customApis/',parameters('Billogram_name'),'')]", defintion["resources"][0]["properties"]["parameters"]["$connections"]["value"]["Billogram"]["id"]);
+            Assert.AreEqual("[concat('/subscriptions/',subscription().subscriptionId,'/resourceGroups/',parameters('Billogram-ResourceGroup'),'/providers/Microsoft.Web/customApis/',parameters('Billogram_api'),'')]", defintion["resources"][0]["properties"]["parameters"]["$connections"]["value"]["Billogram"]["id"]);
 
             Assert.AreEqual("Microsoft.Web/connections", defintion["resources"][1]["type"]);
             Assert.AreEqual("[parameters('logicAppLocation')]", defintion["resources"][1]["location"]);
             Assert.AreEqual("[parameters('Billogram_name')]", defintion["resources"][1]["name"]);
             //subscriptions/89d02439-770d-43f3-9e4a-8b910457a10c/resourceGroups/Messaging/providers/Microsoft.Web/customApis/Billogram
             //subscriptions/fakeecb73-d0ff-455d-a2bf-eae0b300696d/providers/Microsoft.Web/locations/westeurope/managedApis/filesystem
-            Assert.AreEqual("[concat('/subscriptions/',subscription().subscriptionId,'/resourceGroups/',parameters('Billogram-ResourceGroup'),'/providers/Microsoft.Web/customApis/',parameters('Billogram_name'),'')]", defintion["resources"][1]["properties"]["api"]["id"]);
+            Assert.AreEqual("[concat('/subscriptions/',subscription().subscriptionId,'/resourceGroups/',parameters('Billogram-ResourceGroup'),'/providers/Microsoft.Web/customApis/',parameters('Billogram_api'),'')]", defintion["resources"][1]["properties"]["api"]["id"]);
         }
 
         [TestMethod()]
