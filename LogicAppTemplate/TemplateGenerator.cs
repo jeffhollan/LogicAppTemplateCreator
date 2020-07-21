9f721eaf0205b62f9e167fabe6f191c1e0c567e5
Mattias <m.logdberg@gmail.com>
2020-07-21 17:11:56 +0200

    fixed service bus connection issue

--- LogicAppTemplate/TemplateGenerator.cs
+++ LogicAppTemplate/TemplateGenerator.cs
@@ -795,7 +795,7 @@ namespace LogicAppTemplate
                             var sb_resource_group_param = AddTemplateParameter($"servicebus_resourceGroupName", "string", "REPLACE__servicebus_rg");
                             var servicebus_auth_name_param = AddTemplateParameter($"servicebus_accessKey_name", "string", "RootManageSharedAccessKey");
 
-                            connectionParameters.Add(parameter.Name, $"[listkeys(resourceId(parameters('servicebus_rg'),'Microsoft.ServiceBus/namespaces/authorizationRules', parameters('servicebus_namespace_name'), parameters('servicebus_auth_name')), '2017-04-01').primaryConnectionString]");
+                            connectionParameters.Add(parameter.Name, $"[listkeys(resourceId(parameters('{sb_resource_group_param}'),'Microsoft.ServiceBus/namespaces/authorizationRules', parameters('{namespace_param}'), parameters('{servicebus_auth_name_param}')), '2017-04-01').primaryConnectionString]");
 
                         }
                         else if (concatedId.EndsWith("/azureeventgridpublish')]"))
